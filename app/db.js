cb16415c562d6ce6c9d52e0ab17766f7ebab4c6f
Yehuda Tuvia <ytuvia@gmail.com>
2017-04-23 07:24:06 +0300

    populate init

--- app/db.js
+++ app/db.js
@@ -1,4 +1,5 @@
 import mongo from 'mongodb'
+import co from 'co';
 
 const uri = 'mongodb://ec2-54-148-65-153.us-west-2.compute.amazonaws.com:27017/seoai';
 let client = mongo.MongoClient;
@@ -20,6 +21,25 @@ export const insertMany = (docs) => {
 				})
 			}
 		})
+	})
+}
+
+export const page = (limit, last) => {
+	return co(function*(){
+		let db = yield MongoClient.connect(uri);
+		let col = db.collection('websites');
+		let filter = {};
+		if(last){
+			filter = {_id:{$lte: last._id}}
+		}
+		let cursor = yield col.find().limit(limit).sort({_id:-1});
+		let docs = [];
+		while(yield cursor.hasNext()){
+			let doc = cursor.next();
+			docs.push(doc);
+		}
+		db.close();
+		return docs;
 	})
 }
 
