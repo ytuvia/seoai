e0e2191749ff97bcfdcf4da9617e3a23ca5fae42
Yehuda Tuvia <ytuvia@gmail.com>
2017-04-26 15:05:13 +0300

    pop cont

--- app/neo.js
+++ app/neo.js
@@ -1,11 +1,8 @@
 import rpn from 'request-promise-native'
 
+const uri = 'http://ec2-35-167-18-73.us-west-2.compute.amazonaws.com/db/data/transaction/commit'
 
-let options = {
-	uri: 'http://ec2-35-167-18-73.us-west-2.compute.amazonaws.com/db/data/transaction/commit'
-}
-
-const export cypher = (statements) => {
+export const cypher = (statements) => {
 	var options = {
 	    method: 'POST',
 	    uri: uri,
