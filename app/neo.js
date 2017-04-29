e00a7c8a5b17f2d253e80e4dda50bf3c09088a7c
Yehuda Tuvia <ytuvia@gmail.com>
2017-04-29 15:35:40 +0300

    neo user change

--- app/neo.js
+++ app/neo.js
@@ -1,7 +1,7 @@
 import rpn from 'request-promise-native'
 
 const uri = 'http://ec2-35-167-18-73.us-west-2.compute.amazonaws.com:7474/db/data/transaction/commit'
-const user = 'neo4j'
+const user = 'seoai'
 const password = 'Seo1357'
 
 export const cypher = (statements) => {
